8c24c9044f94389a9b3db415730f5aeaa3e9ee18
Joe Kent <thedeadlybutter@gmail.com>
2014-07-24 16:31:14 -0400

    Added dynamic URL support in sockets

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 $(function() {
-    var socket = (location.hostname === 'blackangus.dosomething.org') ? io.connect('blackangus.dosomething.org:3000') : io.connect('localhost:3000');
+    var hostname = 'http://' + location.hostname + ":" + location.port;
+    var socket = io.connect(hostname);
     var picks = [];
 
     var current = 0;
