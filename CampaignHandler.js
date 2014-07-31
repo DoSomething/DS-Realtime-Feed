3063ad8afc2e0eea3392dacbb556fffd194ede82
Joe Kent <thedeadlybutter@gmail.com>
2014-07-31 12:58:40 -0400

    Moved date utility function to Campaign module

--- CampaignHandler.js
+++ CampaignHandler.js
@@ -2,6 +2,14 @@ var request = require('superagent');
 
 var url = "http://beta.dosomething.org/api/v1/content/2401.json";
 
+Date.prototype.daysBetween = function(date1, date2) {
+  var one_day=1000*60*60*24;
+  var date1_ms = date1.getTime();
+  var date2_ms = date2.getTime();
+  var difference_ms = date2_ms - date1_ms;
+  return Math.round(difference_ms/one_day);
+}
+
 /*
  * title      The title of the campaign
  * imageURL   The link to the square high-res campaign photo
--- index.js
+++ index.js
@@ -39,19 +39,6 @@ Date.prototype.subHours = function(h) {
    return this;
 }
 
-Date.prototype.subMinutes = function(m) {
-    this.setTime(this.getTime() - (m*60000));
-    return this;
-}
-
-Date.prototype.daysBetween = function(date1, date2) {
-  var one_day=1000*60*60*24;
-  var date1_ms = date1.getTime();
-  var date2_ms = date2.getTime();
-  var difference_ms = date2_ms - date1_ms;
-  return Math.round(difference_ms/one_day);
-}
-
 io.on('connection', function(socket) {
   console.log("Client connected");
 
