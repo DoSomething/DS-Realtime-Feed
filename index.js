1f2bb9ad28703c4907d52e61e3998bad9eedaf77
Joe Kent <thedeadlybutter@gmail.com>
2014-07-31 11:12:43 -0400

    Quick index cleanup

--- index.js
+++ index.js
@@ -16,15 +16,13 @@ var countFile = require(__dirname + '/count.json');
 
 var fs = require("fs");
 var express = require('express');
-var request = require('superagent');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var request = require('superagent');
 var amqp = require('amqp');
 var parseString = require('xml2js').parseString;
 var PHPUnserialize = require('php-unserialize');
-var cheerio = require('cheerio');
-var stathat = require('stathat');
 
 var totalUsers = countFile.total;
 
@@ -66,14 +64,14 @@ io.on('connection', function(socket) {
 /*
  * Sends an activity message to all connected clients
  */
-function sendActivityMessage(type, message){
+this.sendActivityMessage = function (type, message){
   io.emit(type, message, {for: 'everyone'});
 }
 
 /*
  * Pushes the current user total to the client ticker
  */
-function pushUserTotal(){
+this.pushUserTotal = function (){
   io.emit('ticker', totalUsers, {for: 'everyone'});
 }
 
