2f96ba817a2056f79c2f864de0628a0f5be9796b
Joe Kent <thedeadlybutter@gmail.com>
2015-03-10 15:52:03 -0400

    Integrates socketio service

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 // Get it? It's made of boxes, just like our feed.
 
 console.log("Loading config...");
-this.app_config = require(__dirname + '/config/app_config.json');
+//need to redo this, use ENV files and put them in a diff. private repo -- use forman to start the app, solves everything
 
 console.log("Loading global modules...");
 var express = require('express');
@@ -25,11 +25,17 @@ app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
 console.log("Loading services...");
-var service_drupal = require(__dirname + '/services/drupal.js');
-var service_google = require(__dirname + '/services/google.js');
-var service_message_broker = require(__dirname + '/services/message_broker.js');
-var service_socekt = require(__dirname + '/services/socket.js');
-var service_mobile_commons = require(__dirname + '/services/mobile_commons.js');
+this.service_drupal = require(__dirname + '/services/drupal.js');
+this.service_google = require(__dirname + '/services/google.js');
+this.service_message_broker = require(__dirname + '/services/message_broker.js');
+this.service_socekt = require(__dirname + '/services/socket.js')(http);
+this.service_mobile_commons = require(__dirname + '/services/mobile_commons.js');
+
+console.log("Loading internal modules...");
+this.module_static = require(__dirname + '/modules/static.js');
+this.module_counters = require(__dirname + '/modules/counters.js');
+this.module_events = require(__dirname + '/modules/events.js');
+this.module_campaigns = require(__dirname + '/modules/campaigns.js');
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
---
+++ services/socket.js
@@ -0,0 +1,48 @@
+var io;
+var callbacks = [];
+
+module.exports = function(http){
+  io = require('socket.io')(http);
+  init();
+}
+
+/*
+ * Called after SocketIO initilization.
+ */
+function init(){
+  io.on('connection', function(socket) {
+    notify();
+  });
+}
+
+/*
+ * Registers a callback with the service
+ * Callbacks upon notification will be get passed the message & senders socket.
+ *
+ * @param Function callback
+ */
+function registerCallback(callback, prefix) {
+  callbacks.push(callback);
+}
+
+/*
+ * Broadcast a message to all clients.
+ *
+ * @param String prefix
+ * @param String msg
+ */
+function broadcastMessage(prefix, msg) {
+  io.emit(prefix, msg);
+}
+
+/*
+ * Notifies all of the registered callbacks
+ *
+ * @param String data
+ * @param Object socket
+ */
+function notify(data, socket) {
+  callbacks.forEach(function(element, index, array) {
+    element(data, socket);
+  });
+}
