29da4a7ba73c7f67d4a26a7d70ab6859bdc2a97d
Joe Kent <thedeadlybutter@gmail.com>
2015-03-16 15:25:27 -0400

    Adds CTL counter

--- modules/counters.js
+++ modules/counters.js
@@ -1,3 +1,18 @@
+var request = require('superagent');
+
 module.exports = function(app, router){
+
+  router.get('/ctl', function(req, res) {
+    request
+     .get("http://crisistextline.org/trends/data/messages.txt")
+     .end(function(response){
+        var total = response.text;
+        res.json({total: total});
+     });
+  });
+
+  router.get('/dosomething', function(req, res) {
+    //@TODO
+  });
 
 }
