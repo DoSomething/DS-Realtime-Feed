3dee1214ca2af2e3472b45f2fcb13cca5f6570ae
Joe Kent <thedeadlybutter@gmail.com>
2015-03-19 15:50:02 -0400

    Cleaned up markup and added scss

--- app.js
+++ app.js
@@ -17,8 +17,17 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
+var sassMiddleWare = require('node-sass-middleware');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(
+  sassMiddleWare({
+    src: __dirname + '/sass',
+    dest: __dirname + '/public',
+    outputStyle: 'compressed',
+    debug: true
+  })
+);
 app.use(express.static(__dirname + '/public'));
 
 console.log("Loading services...");
