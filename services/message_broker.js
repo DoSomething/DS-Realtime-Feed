64057be4e1e94b26875f3f02a0de4a40fb0da514
Joe Kent <thedeadlybutter@gmail.com>
2015-04-14 14:57:46 -0400

    1 more log

--- services/message_broker.js
+++ services/message_broker.js
@@ -5,7 +5,7 @@ module.exports = function(router) {
   var message_broker = {};
 
   router.post('/', function(req, res) {
-    console.log(req.data);
+    console.log("BOOM");
     notify(req.data);
     res.json(["OKAY", 200]);
   });
