70efb39c1b9cfc1abe45b62f5e7b7907b27f54db
Joe Kent <thedeadlybutter@gmail.com>
2015-04-14 14:44:54 -0400

    Adds loggers

--- public/js/main.js
+++ public/js/main.js
@@ -169,6 +169,7 @@ $(document).on('ready', function() {
 
   var userBoxIndex = 0;
   function handleUserEvent(data) {
+    console.log(data);
     var container = $('.slide-feed');
     var children = container.children();
     if(userBoxIndex >= children.length){
